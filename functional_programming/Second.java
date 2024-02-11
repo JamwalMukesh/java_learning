f37f5f426c60b767b0ed8754bbd8c78b880de2ec
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-02-11 22:02:09 +0530

    functional programming udemy

---
+++ functional_programming/Second.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class FunctionalProgrammingRunner
+{
+    public static void main(String[] args)
+    {
+        List<String> list = List.of("Apples","Banana","Cat","Dog");
+        // printBasic(list);
+        // printWithFP(list);
+        printWithFPWithFilter(list);
+    }
+
+    private static void printBasic(List<String> list)
+    {
+        for(String str:list)
+        {
+            System.out.println(str);
+        }
+    }
+
+    private static void printWithFP(List<String> list)
+    {
+        list.stream().forEach(element -> System.out.println("element - " + element));
+    }
+
+    private static void printWithFPWithFilter(List<String> list)
+    {
+        list.stream()
+            .filter(element -> element.endsWith("at"))
+            .forEach(element -> System.out.println("element - " + element));
+    }
+}
